06b0e40e242961e4c5dc07678032c48e586619ea
“Rimsha <rimsha.zaib@arbisoft.com>
2023-10-19 16:59:57 +0500

    django tutor plugin app

--- cookiecutter_courses/apps.py
+++ cookiecutter_courses/apps.py
@@ -1,47 +1,20 @@
-# """
-# cookiecutter_courses Django application initialization.
-# """
-
-# from django.apps import AppConfig
-
-
-# class CookiecutterCoursesConfig(AppConfig):
-#     """
-#     Configuration for the cookiecutter_courses Django application.
-#     """
-#     default_auto_field = 'django.db.models.BigAutoField'
-#     name = 'cookiecutter_courses'
-
-
-​
-from django.apps import AppConfig​
+from django.apps import AppConfig
+from edx_django_utils.plugins.constants import (
+    PluginURLs
+)
+from openedx.core.djangoapps.plugins.constants import ProjectType
 class CookiecutterCoursesConfig(AppConfig):
     """
     Configuration for the list_and_filter Django application.
     """
-​
     name = 'cookiecutter_courses'
-    plugin_app = {
-        'url_config': {
-            'lms.djangoapp': {
-                'namespace': 'cookiecutter_courses',
-                'relative_path': 'urls',
-            }
-        },
-        'settings_config': {
-            'lms.djangoapp': {
-                'common': {'relative_path': 'settings'},
-            }
-        },
-    } 
 
-​
-    # plugin_app = {
-    #     PluginURLs.CONFIG: {
-    #         ProjectType.LMS: {
-    #             PluginURLs.NAMESPACE: 'cookiecutter_courses',
-    #             PluginURLs.REGEX: r'^api/cookiecutter_courses/',
-    #             PluginURLs.RELATIVE_PATH: 'urls',
-    #         }
-    #     }
-    # }
+    plugin_app = {
+        PluginURLs.CONFIG: {
+            ProjectType.LMS: {
+                PluginURLs.NAMESPACE: 'cookiecutter_courses',
+                PluginURLs.REGEX: r'^api/cookiecutter_courses/',
+                PluginURLs.RELATIVE_PATH: 'urls',
+            }
+        }
+    }
