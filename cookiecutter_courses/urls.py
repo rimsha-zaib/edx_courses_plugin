9b2dfcb28367415197a5e0e382ac57b7fc3d1bfc
“Rimsha <rimsha.zaib@arbisoft.com>
2023-10-19 23:32:11 +0500

    tutor django plugin

--- cookiecutter_courses/urls.py
+++ cookiecutter_courses/urls.py
@@ -3,11 +3,12 @@ from django.urls import path
 from django.contrib import admin
 
 from . import views  # Import your views module
-from cookiecutter_courses.views import get_courses
+from cookiecutter_courses.views import get_courses, CourseListAPIView
 
 urlpatterns = [
     path(
         'list/', get_courses, name='get_courses_list'
-    )
+    ),
+    path('v1/list/', CourseListAPIView.as_view(), name='course_list')
 ]
 #helllo
--- cookiecutter_courses/views.py
+++ cookiecutter_courses/views.py
@@ -3,10 +3,19 @@ from rest_framework.response import Response
 from .models import Courses
 from rest_framework.generics import ListAPIView
 from .serializers import CoursesSerializer
+from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
+from lms.djangoapps.course_api.serializers import CourseSerializer
 
 @api_view(['GET'])
 def get_courses(request):
     courses = Courses.objects.all()
     serializer = CoursesSerializer(courses, many=True)
     return Response({'courses': serializer.data})
+class CourseListAPIView(ListAPIView):
 
+    queryset = CourseOverview.get_all_courses().prefetch_related(
+        'modes',
+    ).select_related(
+        'image_set'
+    )
+    serializer_class = CourseSerializer
