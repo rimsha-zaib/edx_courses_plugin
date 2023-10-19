9d8191fc08b1de4f567fc5dd202d8f42ad8377a6
“Rimsha <rimsha.zaib@arbisoft.com>
2023-10-19 22:06:38 +0500

    django app plugin

--- cookiecutter_courses/urls.py
+++ cookiecutter_courses/urls.py
@@ -3,10 +3,9 @@ from django.urls import path
 from django.contrib import admin
 
 from . import views  # Import your views module
-from cookiecutter_courses.views import get_courses, CourseListAPIView
+from cookiecutter_courses.views import get_courses
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path(
         'list/', get_courses, name='get_courses_list'
     )
